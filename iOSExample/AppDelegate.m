f52339313454b3c06b82d8bedecb356e60305f28
Frank Schmitt <frank@apptentive.com>
2016-07-28 17:55:30 -0700

    Add basic Apptentive features to app

--- iOSExample/AppDelegate.m
+++ iOSExample/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "Apptentive.h"
 
 @interface AppDelegate ()
 
@@ -16,7 +17,8 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-	// Override point for customization after application launch.
+	[Apptentive sharedConnection].APIKey = @"<Your Apptentive API Key>";
+
 	return YES;
 }
 
--- iOSExample/ViewController.m
+++ iOSExample/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "Apptentive.h"
 
 @interface ViewController ()
+
+@property (strong, nonatomic) IBOutlet UITableViewCell *messageCenterCell;
 
 @end
 
@@ -17,11 +20,21 @@
 - (void)viewDidLoad {
 	[super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+
+	self.messageCenterCell.accessoryView = [[Apptentive sharedConnection] unreadMessageCountAccessoryView:YES];
 }
 
 - (void)didReceiveMemoryWarning {
 	[super didReceiveMemoryWarning];
 	// Dispose of any resources that can be recreated.
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+	if (indexPath.section == 0) {
+		[[Apptentive sharedConnection] engage:@"event" fromViewController:self];
+	} else {
+		[[Apptentive sharedConnection] presentMessageCenterFromViewController:self];
+	}
 }
 
 @end
