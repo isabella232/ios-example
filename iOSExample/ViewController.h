9e1cc88c5ec46f1ae1e5957a110dd51084b1d42a
Frank Schmitt <frank@apptentive.com>
2016-07-28 17:35:48 -0700

    Add single-view template app generated by Xcode

---
+++ iOSExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSExample
+//
+//  Created by Frank Schmitt on 7/28/16.
+//  Copyright © 2016 Apptentive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOSExample/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iOSExample
+//
+//  Created by Frank Schmitt on 7/28/16.
+//  Copyright © 2016 Apptentive. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+@end
